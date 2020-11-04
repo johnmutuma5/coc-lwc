3a5aa2673f2af3e4e132f26f1872c9f9dba63301
John Mutuma <johnmutuma5@gmail.com>
2020-11-04 15:12:15 +0300

    log success client ready

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export async function activate(context: ExtensionContext) {
 
   console.info('LWC workspace detected. Starting LWC Server now!!');
   const client: LanguageClient = setupLwcServerClient(context);
+  client.onReady().then(() => console.info('Success!! LWC client ready'));
   // Push the disposable to the context's subscriptions so that the
   // client can be deactivated on extension deactivation
   context.subscriptions.push(client.start());
